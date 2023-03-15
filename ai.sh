c53897d7e96222e72ae8eddc9a56617d5d1986fc
mingbetter <1745463810@qq.com>
2023-03-15 15:19:19 +0800

    Update ai.sh
    一键安装

--- ai.sh
+++ ai.sh
@@ -1,15 +1,17 @@
 #一键安装vpn
-# curl -fsSL https://git.io/vpnstart -o vpn.sh && sudo sh vpn.sh
-# #配置nginx
-# sudo apt-get -y install nginx
-# sudo systemctl status nginx
-# sudo ufw allow 'Nginx Full'
-# sudo systemctl restart nginx
-# #移动
-# cp /root/vpnclient.mobileconfig /var/www/html
-# chmod 777 /var/www/html/vpnclient.mobileconfig
+curl -fsSL https://git.io/vpnstart -o vpn.sh && sudo sh vpn.sh
+#配置nginx
+sudo apt-get -y install nginx
+sudo systemctl status nginx
+sudo ufw allow 'Nginx Full'
+sudo systemctl restart nginx
+#移动凭证
+cp /root/vpnclient.mobileconfig /var/www/html
+chmod 777 /var/www/html/vpnclient.mobileconfig
+#打印地址
 ipstr=""
 ipstr=`curl ifconfig.me`
-echo  "\n 描述文件如下："
+echo "描述文件如下："
+echo ""
 echo http://$ipstr/vpnclient.mobileconfig
 echo ""
